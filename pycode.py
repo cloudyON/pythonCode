a139705d50789eb9c99cc4a98eb6ad67a33a8f5c
cloud7788 <{ydg5665@gmail.com}>
2021-02-04 13:17:43 +0900

    first commit

--- pycode.py
+++ pycode.py
@@ -1,6 +1,6 @@
-from random import randrange
+from random import randrange #난수 생성
 
-def bigNum(a):
+def bigNum(a): #제일 큰 수를 찾는다
     inNum = a
     num = 0
     for n in inNum:
@@ -9,10 +9,10 @@ def bigNum(a):
 
     return num
 
-if __name__ == "__main__":
+if __name__ == "__main__": #시작
     array = []
-    for i in range(randrange(1,11)):
-        array.append(randrange(1,101))
+    for i in range(randrange(1,11)): 
+        array.append(randrange(1,101)) #난수 범위 정하기
     
 
     print(bigNum(array))
