64280169161624b3b8eb65e2d8ce3b0d7e832a43
unknown <ydg5665@gmail.com>
2021-02-10 20:28:06 +0900

    Incomplete python project

--- airportSystem.py
+++ airportSystem.py
@@ -1,7 +1,15 @@
 class passenger():
     def __init__(self):
         self.money = 0
-        self.reservation = 0
+        self.schedule = 0
+        self.reserv = []
+
+    def reservation(self, airline):
+        print("this code is incomplete")
+        # Incomplete
+        # Incomplete
+
+
 
 
 class airport_system():
@@ -11,21 +19,43 @@ class airport_system():
 
 
 
+
+
+
 class airline():
     def __init__(self):
         self.airlines = ['sun air', 'korea air', 'mountain air', 'cloud air', '1st\'s air', 'foru air']
-        self.al_Schedule = {'sun air':['1/12','1/14','1/19','1/21','2/1','2/15'],
-                            'korea air':['1/30','2/14','2/15','2/16'],
-                            'moutain air':['1/30', '2/21','3/21'], # mountain air 는 아주 고급이라 조금밖에 운행 안합니다.
-                            'cloud air':['1/12','1/15','1/16','1/21','2/9','2/10','3/1','3/12'],
-                            '1st\'s air':['1/21','1/23','2/1','2/3','2/5','2/7','2/9'],
-                            'foru air':['1/22','1/23','2/5','2/7','2/9','2/14','2/21']}
+        self.al_Schedule = {'sun air': ['1/12', '1/14', '1/19', '1/21', '2/1', '2/15'],
+                            'korea air': ['1/30', '2/14', '2/15', '2/16'],
+                            'moutain air': ['1/30', '2/21','3/21'],  # mountain air 는 아주 고급이라 조금밖에 운행 안합니다.
+                            'cloud air': ['1/12', '1/15', '1/16', '1/21', '2/9', '2/10', '3/1', '3/12'],
+                            '1st\'s air': ['1/21', '1/23', '2/1', '2/3', '2/5', '2/7', '2/9'],
+                            'foru air': ['1/22', '1/23', '2/5', '2/7', '2/9', '2/14', '2/21']}
 
-    def del_airline(self, delt):
+    def del_airline(self, dele):
         try :
-            del self.airlines[self.airlines.index(delt)]
+            del self.airlines[self.airlines.index(dele)]
+            del self.al_Schedule[dele]
         except ValueError:
-            print(f"That airline : \'{delt}\' does not exist.")
+            print(f"That airline : \'{dele}\' does not exist.")
+
+    def create_airlines(self, name : ''):
+        if name not in self.airlines:
+            self.airlines.append(name)
+            self.al_Schedule[name] = []
+            print("항공사가 추가되었습니다.")
+
+        else :
+            print("이미 다른 항공사가 있습니다.")
+
+    def create_schedule(self, airline : 'integer',schedule : 'string'):
+        if airline in self.airlines :
+            self.al_Schedule[airline].appned(schedule)
+
+        else :
+            print(f"{airline} 항공사는 존재하지 않습니다.")
+
+
 
 
 
